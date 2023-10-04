ea0888acb70c4ce0a9959aaf374ad2996db38e79
Adv1ce1 <135262373+Adv1ce1@users.noreply.github.com>
2023-10-04 18:25:44 +0300

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,9 +6,9 @@ from catalog.views import ProductListView, ContactDetailView, ProductDetailView,
 app_name = 'catalog'
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('contacts/', ContactDetailView.as_view(), name='contacts'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<str:name>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/<str:name>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<str:name>/delete/', ProductDeleteView.as_view(), name='product_delete'),
-    path('contacts/', ContactDetailView.as_view(), name='contacts'),
 ]
